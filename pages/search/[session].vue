865b61c352981ad73eab33477d2b75ffbf590a79
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-01-09 17:14:18 +0200

    Search results page skeleton

---
+++ pages/search/[session].vue
@@ -0,0 +1,14 @@
+<template>
+  <v-main></v-main>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const route = useRoute();
+
+const result = ref();
+
+onBeforeMount(async () => {
+  result.value = await useFetch(`${config.public.apiBase}/search/${route.params.session}`);
+});
+</script>
