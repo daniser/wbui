db39d1953eb879f38fc40bfc6718f843b9fe3ed5
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-01-09 15:21:50 +0200

    Vuetify setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify']
+  },
   devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    }
+  ],
   runtimeConfig: {
     apiSecret: '',
     public: {
@@ -10,5 +22,12 @@ export default defineNuxtConfig({
   typescript: {
     strict: true,
     typeCheck: true
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
   }
 })
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-main>
+    <v-form @submit.prevent="onSubmit">
+      <v-container>
+        <v-row>
+          <v-col><v-autocomplete v-model="fields.from" label="From" :items="locations" /></v-col>
+          <v-col><v-autocomplete v-model="fields.to" label="To" :items="locations" /></v-col>
+          <v-col>
+            <v-text-field v-model="formattedDate" readonly label="Date">
+              <v-overlay
+                  open-on-click
+                  close-on-content-click
+                  activator="parent"
+                  location-strategy="connected"
+                  location="bottom center"
+                  origin="auto"
+              >
+                <v-date-picker
+                    v-model="dateTo"
+                    landscape
+                    show-adjacent-months
+                    hide-header
+                    color="primary"
+                    elevation="12"
+                />
+              </v-overlay>
+            </v-text-field>
+          </v-col>
+          <v-col><v-btn type="submit" size="x-large" color="primary">Search</v-btn></v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-main>
+</template>
+
+<script setup lang="ts">
+import { useDate } from 'vuetify';
+
+const date = useDate();
+const config = useRuntimeConfig();
+
+const locations = ['MOW', 'LED', 'KGD'];
+
+const dateTo = ref();
+const result = ref();
+
+const formattedDate = computed(() => date.format(dateTo.value, 'keyboardDate'));
+
+const fields = reactive({
+  from: '',
+  to: '',
+  date: formattedDate.value
+});
+
+const onSubmit = async () => {
+  result.value = await useFetch(`${config.public.apiBase}/search`, {
+    method: 'post',
+    body: new URLSearchParams(fields)
+  });
+};
+</script>
