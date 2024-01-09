c6d1caf17bc59965be0f6be9cca5857ae952ed63
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-01-09 10:59:19 +0200

    Remove Ionic and VeeValidate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/ionic',
-    [
-      '@vee-validate/nuxt',
-      {
-        autoImports: true,
-        componentNames: {
-          Form: 'VeeForm',
-          Field: 'VeeField',
-          FieldArray: 'VeeFieldArray',
-          ErrorMessage: 'VeeErrorMessage'
-        }
-      }
-    ]
-  ],
   runtimeConfig: {
     apiSecret: '',
     public: {
       apiBase: ''
     }
   },
-  ssr: false,
   typescript: {
     strict: true,
-    tsConfig: {
-      compilerOptions: {
-        verbatimModuleSyntax: false
-      }
-    },
     typeCheck: true
   }
 })
