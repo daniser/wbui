8dc4361a5bec07f26161c9006925d66f5899180e
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-02-15 15:56:53 +0200

    FlagIcon component - optionally squared flags

--- components/FlagIcon.vue
+++ components/FlagIcon.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-icon :icon="`fi fi-${country.toLowerCase()}`" class="!opacity-100" />
+  <v-icon :icon="`fi fi-${country.toLowerCase()}${squared ? ' fis' : ''}`" class="!opacity-100" />
 </template>
 
 <script setup lang="ts">
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import type { TCountryCode } from "countries-list";
 
-defineProps<{ country: TCountryCode }>();
+defineProps<{ country: TCountryCode; squared?: boolean }>();
 </script>
