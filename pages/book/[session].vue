61b51735aba30c36998288050b63504959deec36
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-02-28 10:20:36 +0200

    I18n: gender-dependent name & email placeholders

--- pages/book/[session].vue
+++ pages/book/[session].vue
@@ -4,20 +4,14 @@
       <v-container>
         <v-row>
           <v-col cols="2">
-            <v-btn-toggle mandatory divided variant="outlined">
-              <v-btn icon="mdi-face-man" value="male" :title="$t('male')" />
-              <v-btn icon="mdi-face-woman" value="female" :title="$t('female')" />
+            <v-btn-toggle v-model="fields.gender" mandatory divided variant="outlined">
+              <v-btn icon="mdi-face-man" value="MALE" :title="$t('male')" />
+              <v-btn icon="mdi-face-woman" value="FEMALE" :title="$t('female')" />
             </v-btn-toggle>
           </v-col>
-          <v-col>
-            <v-text-field :label="$t('last_name')" :placeholder="$t('last_name_placeholder')" />
-          </v-col>
-          <v-col>
-            <v-text-field :label="$t('first_name')" :placeholder="$t('first_name_placeholder')" />
-          </v-col>
-          <v-col>
-            <v-text-field :label="$t('middle_name')" :placeholder="$t('middle_name_placeholder')" />
-          </v-col>
+          <v-col><v-text-field :label="$t('last_name')" :placeholder="lastNamePlaceholder" /></v-col>
+          <v-col><v-text-field :label="$t('first_name')" :placeholder="firstNamePlaceholder" /></v-col>
+          <v-col><v-text-field :label="$t('middle_name')" :placeholder="middleNamePlaceholder" /></v-col>
           <v-col>
             <v-text-field type="date" :label="$t('date_of_birth')" />
           </v-col>
@@ -55,7 +49,7 @@
             <v-text-field
               type="email"
               :label="$t('email')"
-              :placeholder="$t('email_placeholder')"
+              :placeholder="emailPlaceholder"
               prepend-inner-icon="mdi-at"
             />
           </v-col>
@@ -68,10 +62,27 @@
 
 <script setup lang="ts">
 import type { TCountryCode } from "countries-list";
+import type { Gender } from "~/types";
 
 const { t } = useI18n();
 
 const config = useRuntimeConfig();
+
+const lastNamePlaceholder = computed(() =>
+  fields.gender !== "FEMALE" ? t("last_name_placeholder") : t("last_name_placeholder_female"),
+);
+
+const firstNamePlaceholder = computed(() =>
+  fields.gender !== "FEMALE" ? t("first_name_placeholder") : t("first_name_placeholder_female"),
+);
+
+const middleNamePlaceholder = computed(() =>
+  fields.gender !== "FEMALE" ? t("middle_name_placeholder") : t("middle_name_placeholder_female"),
+);
+
+const emailPlaceholder = computed(() =>
+  fields.gender !== "FEMALE" ? t("email_placeholder") : t("email_placeholder_female"),
+);
 
 const preferredCountries = t("preferred_countries")
   .split(",")
@@ -81,7 +92,8 @@ const excludedCountries: TCountryCode[] = t("excluded_countries")
   .split(",")
   .filter((x) => x) as TCountryCode[];
 
-const fields = reactive<{ citizenship?: TCountryCode; phone: string }>({
+const fields = reactive<{ gender?: Gender; citizenship?: TCountryCode; phone: string }>({
+  gender: undefined,
   citizenship: undefined,
   phone: "",
 });
