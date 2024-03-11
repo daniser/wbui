8cd086edfca5807a39da277cb6d02ac135e8fa76
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-03-11 11:57:43 +0200

    Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ["vue-debounce"],
+  },
   devtools: { enabled: true },
   modules: [
     [
