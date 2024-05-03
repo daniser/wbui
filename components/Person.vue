c45d5a473d6340a0abc2dbaa516c4d9d605d3400
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-05-03 12:27:12 +0200

    Person component fixes

--- components/Person.vue
+++ components/Person.vue
@@ -1,22 +1,20 @@
 <template>
-  <v-form @submit.prevent="$emit('update:modelValue', person)">
-    <v-card variant="tonal">
-      <v-card-text>
-        <v-text-field v-model="person.name" @input.stop variant="underlined" />
-      </v-card-text>
-      <v-card-actions>
-        <v-btn v-if="dirty" type="submit">Save</v-btn>
-        <v-btn v-if="dirty" @click="person.$refresh()">Reset</v-btn>
-        <v-btn>Add</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-form>
+  <v-confirm-edit v-model="person">
+    <template #default="{ model: proxyModel, actions }">
+      <v-card variant="tonal">
+        <v-card-text>
+          <v-text-field v-model="proxyModel.value.name" variant="underlined" />
+        </v-card-text>
+        <v-card-actions>
+          <component :is="actions" />
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-confirm-edit>
 </template>
 
 <script setup lang="ts">
 import type Person from "~/models/Person";
 
 const person = defineModel<Person>({ required: true });
-
-const dirty = computed(() => person.value.$isDirty());
 </script>
--- pages/profile/passengers.vue
+++ pages/profile/passengers.vue
@@ -1,8 +1,8 @@
 <template>
   <v-main>
     <v-list>
-      <v-list-item v-for="(person, i) in persons" :key="person.id" class="mx-2">
-        <Person v-model="persons[i]" />
+      <v-list-item v-for="person in persons" :key="person.value.id" class="mx-2">
+        <Person v-model="person.value" />
       </v-list-item>
     </v-list>
     <v-btn color="primary" class="m-2">New</v-btn>
@@ -11,6 +11,7 @@
 
 <script setup lang="ts">
 import PersonRepository from "~/repositories/PersonRepository";
+import type Person from "~/models/Person";
 
 definePageMeta({
   layout: "profile",
@@ -19,7 +20,18 @@ definePageMeta({
 
 const personRepo = computed(() => useRepo(PersonRepository));
 
-const persons = computed(() => personRepo.value.all());
+const persons = computed(() =>
+  personRepo.value.all().map((person) =>
+    computed({
+      get() {
+        return person;
+      },
+      set(newValue) {
+        personRepo.value.save(newValue);
+      },
+    }),
+  ),
+);
 
 onMounted(async () => {
   await personRepo.value.fetch();
