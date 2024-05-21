4e8920032d0ccfb234234ed9a9aed04f40165f4c
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-05-21 17:43:06 +0200

    Birth certificate mask adjustments

--- utils/masks/birth_certificate.ts
+++ utils/masks/birth_certificate.ts
@@ -1,42 +1,43 @@
 import type { MaskitoOptions } from "@maskito/core";
+import type { ElementState } from "@maskito/core/src/lib/types";
+
+const slice = (elementState: ElementState) => [
+  elementState.value.slice(0, elementState.selection[0]),
+  elementState.value.slice(...elementState.selection),
+  elementState.value.slice(elementState.selection[1]),
+];
 
 const birthCertificateMask: MaskitoOptions = {
-  mask: /^[IVXLCDM]{1,4}(-[А-ЯЁ]{0,2}( \d{0,6})?)?$/,
+  mask: /^[IVXLCDM]{0,4}(-[А-ЯЁ]{0,2})?( \d{0,6})?$/,
   preprocessors: [
     ({ elementState, data }, actionType) =>
-      actionType === "insert" && /^[А-ЯЁ]$/i.test(data) && /[IVXLCDM]$/.test(elementState.value)
-        ? {
-            elementState: { ...elementState, value: elementState.value + "-" },
-            data: "-" + data,
-          }
+      actionType === "insert" && /^[А-ЯЁ]$/i.test(data) && /[IVXLCDM]$/.test(slice(elementState)[0])
+        ? { elementState, data: "-" + data }
         : { elementState, data },
 
     ({ elementState, data }, actionType) =>
-      actionType === "insert" && /^\d$/.test(data) && /[А-ЯЁ]{2}$/.test(elementState.value)
-        ? {
-            elementState: { ...elementState, value: elementState.value + " " },
-            data: " " + data,
-          }
+      actionType === "insert" && /^\d$/.test(data) && /[А-ЯЁ]{1,2}$/.test(slice(elementState)[0])
+        ? { elementState, data: " " + data }
         : { elementState, data },
 
-    ({ elementState, data }, actionType) =>
-      actionType === "deleteBackward" && /(-[А-ЯЁ]| \d)$/.test(elementState.value)
-        ? {
-            elementState: {
-              selection: elementState.selection.map((x) => --x) as unknown as typeof elementState.selection,
-              value: elementState.value.slice(0, -1),
-            },
-            data,
-          }
-        : { elementState, data },
+    ({ elementState, data }, actionType) => {
+      if (actionType !== "deleteForward" && actionType !== "deleteBackward") {
+        return { elementState, data };
+      }
 
-    ({ elementState, data }) => ({
-      elementState: {
-        ...elementState,
-        value: elementState.value.toUpperCase(),
-      },
-      data: data.toUpperCase(),
-    }),
+      const [prefix, selection, suffix] = slice(elementState);
+      const remains = selection.replace(/[^ -]/g, "");
+      const value = (prefix + remains + suffix).replace(/[ -]*$/g, "");
+      let caretPos = actionType === "deleteForward" ? remains.length : 0;
+      caretPos = Math.min(elementState.selection[0] + caretPos, value.length);
+
+      return {
+        elementState: { value, selection: [caretPos, caretPos] },
+        data,
+      };
+    },
+
+    ({ elementState, data }) => ({ elementState, data: data.toUpperCase() }),
   ],
 };
 
