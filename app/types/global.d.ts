4fe474fd03332d278b60ff65553c9e70669613b2
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-09-01 18:44:01 +0200

    Update dependencies

--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -1,16 +1,21 @@
 import type { AccountResponse, UserResponse } from "~/types/auth";
+import type { $Fetch } from "ofetch";
 
 declare module "#app" {
   interface NuxtApp {
     $user: UserResponse;
     $account: AccountResponse;
+    $api: $Fetch;
+    $capi: $Fetch;
   }
 }
 
-declare module "@vue/runtime-core" {
+declare module "vue" {
   interface ComponentCustomProperties {
     $user: UserResponse;
     $account: AccountResponse;
+    $api: $Fetch;
+    $capi: $Fetch;
   }
 }
 
