24998b9c7516d5dbca527f0c03d327bbf1d93ccb
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-04-01 10:20:15 +0200

    Update dependencies

--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,7 +1,7 @@
 import { defineVuetifyConfiguration } from "vuetify-nuxt-module/custom-configuration";
 
 export default defineVuetifyConfiguration({
-  labComponents: ["VDateInput", "VNumberInput"],
+  labComponents: ["VDateInput"],
   locale: {
     locale: "ru",
     fallback: "en",
