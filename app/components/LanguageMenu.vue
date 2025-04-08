15c1df40f582ef36298ea2e4ab0766376f9ce332
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-04-08 12:25:58 +0200

    Update dependencies

--- app/components/LanguageMenu.vue
+++ app/components/LanguageMenu.vue
@@ -2,19 +2,23 @@
   <v-menu>
     <template #activator="{ props }">
       <v-btn v-bind="props" :title="$t('language')">
-        <template v-if="currentLocale?.flag" #prepend><FlagIcon :country="currentLocale.flag" /></template>
+        <template v-if="currentLocale?.flag" #prepend>
+          <FlagIcon :country="currentLocale.flag as TCountryCode" />
+        </template>
         {{ currentLocale?.name }}
       </v-btn>
     </template>
     <v-list>
       <v-list-item v-for="item in locales" :key="item.code" :to="switchLocalePath(item.code)" :title="item.name">
-        <template v-if="item.flag" #prepend><FlagIcon :country="item.flag" /></template>
+        <template v-if="item.flag" #prepend><FlagIcon :country="item.flag as TCountryCode" /></template>
       </v-list-item>
     </v-list>
   </v-menu>
 </template>
 
 <script setup lang="ts">
+import type { TCountryCode } from "countries-list";
+
 const { locale, locales } = useI18n();
 const switchLocalePath = useSwitchLocalePath();
 
--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,7 +1,6 @@
 import { defineVuetifyConfiguration } from "vuetify-nuxt-module/custom-configuration";
 
 export default defineVuetifyConfiguration({
-  labComponents: ["VDateInput"],
   locale: {
     locale: "ru",
     fallback: "en",
